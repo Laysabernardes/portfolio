a8b593cc0a421996f3dcb1d53703f2b1b4be06e5
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-07-24 18:29:03 -0300

    feat: Create of the CRUD of user for application

---
+++ back-end/src/controllers/user.controller.js
@@ -0,0 +1,79 @@
+import userService from "../services/user.service.js";
+    
+const create = async (req, res) => {
+    try {
+        const { name, username, email, password} = req.body;
+
+        if (!name || !username || !email || !password) {
+            res.status(400).send({ message: "Preencha todos os campos para o registro." });
+        }
+
+        const user = await userService.createService(req.body);
+
+        if (!user) {
+            return res.status(400).send({ message: "Erro ao criar o usuário." });
+        }
+
+        res.status(201).send({
+            message: "Usuário criado com sucesso",
+            user: {
+                
+                id: user._id,
+                name,
+                username,
+                email,
+            }
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.mensage })
+    }
+}
+
+const findAll = async (req, res) => {
+    try {
+        const users = await userService.findAllService();
+
+        if (users.length === 0) {
+            return res.status(400).send({ message: "Não há usuários registrados." });
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(500).send({ message: err.mensage })
+    }
+};
+
+const findById = async (req, res) => {
+
+    try {
+        const user = req.user
+        res.send(user);
+    } catch (err) {
+        res.status(500).send({ message: err.mensage })
+    }
+}
+
+const update = async (req, res) => {
+    try {
+        const { name, username, email, password} = req.body;
+
+        if (!name && !username && !email && !password) {
+            res.status(400).send({ message: "Envie pelo menos um campo para a atualização." });
+        };
+
+        const { id, user } = req;
+
+        await userService.updateService(
+            id,
+            name,
+            username,
+            email,
+            password,
+        );
+
+        res.send({ message: "Usuario foi atualizado com sucesso!" });
+    } catch (err) {
+        res.status(500).send({ message: err.mensage })
+    }
+};
+
+export default { create, findAll, findById, update };
