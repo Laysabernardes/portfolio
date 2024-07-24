4f22aea71d379787ef3cfc4ac0f700ae4f3260bc
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-07-24 20:23:46 -0300

    feat: create the service for user

---
+++ back-end/src/services/user.service.js
@@ -0,0 +1,22 @@
+import User from "../models/User.js";
+
+const createService = (body) => User.create(body);
+const findAllService = () => User.find();
+const findByIdService = (id) => User.findById(id);
+const updateService = (id, name, email, password) => {
+    return User.findOneAndUpdate(
+      { _id: id }, 
+      {           
+        name,
+        email,
+        password,
+      }
+    );
+  };
+
+export default {
+    createService,
+    findAllService,
+    findByIdService,
+    updateService
+};
