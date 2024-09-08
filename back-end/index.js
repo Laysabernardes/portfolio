a266126c300ab8c5e49108208176c22c1351aaa4
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-09-07 22:06:39 -0300

    feat: create the doc swagger

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,7 @@ import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import tecnologiesRoute from "./src/routes/technologies.route.js";
 import projectsRoute from "./src/routes/projects.route.js";
+import swaggerRoute from "./src/routes/swagger.router.js";
 
 dotenv.config();
 
@@ -14,9 +15,11 @@ const port = process.env.PORT || 3001;
 
 connectDatabase();
 app.use(express.json());
+
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/tec", tecnologiesRoute);
 app.use("/proj", projectsRoute);
+app.use("/doc", swaggerRoute);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
