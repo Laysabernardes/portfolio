d51ec35ddf136ab3a9c56d029b07d96fd2f54785
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-09-18 23:39:31 -0300

    feat: function for boton dark-light

--- front-end/src/components/ThemeToggleButton.js
+++ front-end/src/components/ThemeToggleButton.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/ThemeToggleButton.css';
-import {sun, moon} from "../assets/images.js";
+import { sun, moon } from "../assets/images.js";
 
-function ThemeToggleButton() {
+function ThemeToggleButton({ onThemeChange }) {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
 
     useEffect(() => {
@@ -10,22 +10,29 @@ function ThemeToggleButton() {
         if (savedTheme) {
             setIsDarkTheme(savedTheme === 'dark');
             document.body.classList.toggle('dark-theme', savedTheme === 'dark');
+            onThemeChange(savedTheme === 'dark'); // Envia o estado do tema ao Navbar
         }
-    }, []);
+    }, [onThemeChange]);
 
     const toggleTheme = () => {
-        setIsDarkTheme(!isDarkTheme);
         const newTheme = !isDarkTheme ? 'dark' : 'light';
+        setIsDarkTheme(!isDarkTheme);
         document.body.classList.toggle('dark-theme', newTheme === 'dark');
         localStorage.setItem('theme', newTheme);
+        onThemeChange(newTheme === 'dark'); // Atualiza o estado do tema no Navbar
     };
 
     return (
         <div className="theme-toggle">
-            <input type="checkbox" id="darkmode-toggle" />
+            <input
+                type="checkbox"
+                id="darkmode-toggle"
+                checked={isDarkTheme}
+                onChange={toggleTheme} 
+            />
             <label htmlFor="darkmode-toggle">
-                <img src={sun} alt= "Sun icon" className="sun"/>
-                <img src={moon} alt="Moon Icon" className="moon" />
+                <img src={sun} alt="Sun icon" className="sun" />
+                <img src={moon} alt="Moon icon" className="moon" />
             </label>
             <div className="background"></div>
         </div>
