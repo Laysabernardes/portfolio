aa15d1b9dab70717f06d0b453056e4f907628e43
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-10-16 11:17:49 -0300

    feat: created the loader

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import Header from "../components/common/Header/Header.js";
 import Presentation from "../components/sections/Presentation/Presentation.js";
 import Technologies from '../components/sections/Technologies/Technologies.js';
@@ -6,14 +6,24 @@ import Projects from '../components/sections/Projects/Projects.js';
 import About from '../components/sections/About/About.js';
 import Footer from '../components/common/Footer/Footer.js';
 
+import Loader from '../components/shared/Loader/Loader.js';
 
 const Home = () => {
     const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsLoading(false), 5000); // 2 segundos de espera
+        return () => clearTimeout(timer); 
+    }, []);
 
     // Função para alternar o tema
     const handleThemeChange = (isDark) => {
         setIsDarkTheme(isDark);
     };
+
+    // Renderiza o Loader enquanto a página está carregando
+    if (isLoading) return <Loader />;
 
     return (
         <div>
