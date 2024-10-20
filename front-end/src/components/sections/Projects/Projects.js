cb77541b3aec26f36477a10ba50785edc7ced31e
Laysa Bernardes <laysabernardes.ads@gmail.com>
2024-10-19 22:24:48 -0300

    fix: update loader in projects

--- front-end/src/components/sections/Projects/Projects.js
+++ front-end/src/components/sections/Projects/Projects.js
@@ -9,6 +9,7 @@ import "./Projects.css";
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [direction, setDirection] = useState(0);
 
     useEffect(() => {
         const fetchProjects = async () => {
@@ -33,79 +34,98 @@ function Projects() {
     // Funções para mudar de página
     const nextPage = () => {
         if (currentPage < projects.length) {
+            setDirection(1); // Definindo direção para direita
             setCurrentPage(currentPage + 1);
         }
     };
 
     const prevPage = () => {
         if (currentPage > 1) {
+            setDirection(-1); // Definindo direção para es
             setCurrentPage(currentPage - 1);
         }
+    };
+
+    const variants = {
+        enter: (direction) => ({
+            x: direction === 1 ? -200 : 200, 
+            opacity: 0,
+        }),
+        center: {
+            x: 0,
+            opacity: 1,
+        },
+        exit: (direction) => ({
+            x: direction === 1 ? 200 : -200, // Sai para a esquerda ou direita
+            opacity: 0,
+        }),
     };
 
     return (
 
         <div className="projects-wrapper">
             <AnimatedSection>
-            <h1 className="title-section-project">Projetos.</h1>
-            {projects.length > 0 ? (
-                 <div className="project-card">
-                 <AnimatePresence mode="wait">
-                     <motion.div
-                         key={currentPage} 
-                         initial={{ opacity: 0, x: 50 }}
-                         animate={{ opacity: 1, x: 0 }}
-                         exit={{ opacity: 0, x: -50 }}
-                         transition={{ duration: 0.5 }}
-                     >
-                         <div className="project-container">
-                             <div className="info-projects">
-                                 <p className="categoria-project">{projects[currentPage - 1].category}</p>
-                                 <h2 className="title-project">{projects[currentPage - 1].title}</h2>
-                                 <p className="tech-project">
-                                     {Array.isArray(projects[currentPage - 1].technologies) &&
-                                     projects[currentPage - 1].technologies.length > 0
-                                         ? projects[currentPage - 1].technologies.map(tech => (
-                                             <span className="tech-item-project" key={tech._id}>
-                                                 {tech.name}
-                                             </span>
-                                         ))
-                                         : "Tecnologia não disponível"}
-                                 </p>
-                                 <div>
-                                     <p className="descricao-projects">{projects[currentPage - 1].description}</p>
-                                     <div className="links-project">
-                                         <button>
-                                             <a href={projects[currentPage - 1].demoURL} target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-                                         </button>
-                                         <button>
-                                             <a href={projects[currentPage - 1].repositoryURL} target="_blank" rel="noopener noreferrer">Ver Código</a>
-                                         </button>
-                                     </div>
-                                 </div>
-                             </div>
-                             <div className="image-container-project">
-                                 <img src={projects[currentPage - 1].image} alt={projects[currentPage - 1].title} />
-                             </div>
-                         </div>
-                     </motion.div>
-                 </AnimatePresence>
-             </div>
-            ) : (
-               <div className="container-carregando">
-                   <p className="texto-tech">Carregando!</p>
-                   <div  className="centralizar-spinner">
-                       <div className="spinner"></div>
-                   </div>
-               </div>
-            )}
+                <h1 className="title-section-project">Projetos.</h1>
+                {projects.length > 0 ? (
+                    <div className="project-card">
+                        <AnimatePresence custom={direction} mode="wait">
+                        <motion.div
+                                key={currentPage}
+                                custom={direction}
+                                variants={variants}
+                                initial="enter"
+                                animate="center"
+                                exit="exit"
+                                transition={{ duration: 1 }}
+                            >
+                                <div className="project-container">
+                                    <div className="info-projects">
+                                        <p className="categoria-project">{projects[currentPage - 1].category}</p>
+                                        <h2 className="title-project">{projects[currentPage - 1].title}</h2>
+                                        <p className="tech-project">
+                                            {Array.isArray(projects[currentPage - 1].technologies) &&
+                                                projects[currentPage - 1].technologies.length > 0
+                                                ? projects[currentPage - 1].technologies.map(tech => (
+                                                    <span className="tech-item-project" key={tech._id}>
+                                                        {tech.name}
+                                                    </span>
+                                                ))
+                                                : "Tecnologia não disponível"}
+                                        </p>
+                                        <div>
+                                            <p className="descricao-projects">{projects[currentPage - 1].description}</p>
+                                            <div className="links-project">
+                                                <button>
+                                                    <a href={projects[currentPage - 1].demoURL} target="_blank" rel="noopener noreferrer">Ver Projeto</a>
+                                                </button>
+                                                <button>
+                                                    <a href={projects[currentPage - 1].repositoryURL} target="_blank" rel="noopener noreferrer">Ver Código</a>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="image-container-project">
+                                        <img src={projects[currentPage - 1].image} alt={projects[currentPage - 1].title} />
+                                    </div>
+                                </div>
+                            </motion.div>
+                        </AnimatePresence>
+                    </div>
+                ) : (
+                    <div className="container-carregando-project">
+                        <p className="texto-tech">Carregando!</p>
+                        <div className="centralizar-spinner">
+                            <div className="spinner"></div>
+                        </div>
+                    </div>
+                )}
 
-            {/* Controles de paginação */}
-            <div className="pagination-controls">
-                <button onClick={prevPage} disabled={currentPage === 1}> Anterior</button>
-                <span>{currentPage} de {projects.length}</span>
-                <button onClick={nextPage} disabled={currentPage === projects.length}>Proxíma</button>
-            </div>
+                {/* Controles de paginação */}
+                <div className="pagination-controls">
+                    <button onClick={prevPage} disabled={currentPage === 1}> Anterior</button>
+                    <span>{currentPage} de {projects.length}</span>
+                    <button onClick={nextPage} disabled={currentPage === projects.length}>Proxíma</button>
+                </div>
             </AnimatedSection>
         </div>
     );
